5553568df1e73f038c05bfab7ea176c1c4ec736c
uk45h <l.leszewski@gmail.com>
2021-09-20 12:19:56 +0000

    some updates

--- SASStudioPython.py
+++ SASStudioPython.py
@@ -1,6 +1,17 @@
-# Initialize the list
-weekdays = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday","Friday", "Saturday"]
-print("Seven Weekdays are:\n")
-# Iterate the list using for loop
-for day in range(len(weekdays)):
-    print(weekdays[day])
+import os, sys
+sys.path
+
+input_table = 'SASHELP.CLASS'
+output_table = 'WORK.PYTHONOUT'
+
+dfin = SAS.sd2df(input_table)
+
+print("input data shape is:", dfin.shape)
+
+dfout = dfin.transpose()
+
+print("output data shae is:", dfout.shape)
+
+SAS.df2sd(dfout, output_table)
+
+SAS.submit('proc print  data=work.pythonout;run;')
